13b59bed2d3007afc2ccceca000a938001982017
darkboy <zenglin12345@gmail.com>
2020-10-05 12:58:11 +0800

    Add road placing function

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -4,25 +4,34 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
+
 public class UIManager : MonoBehaviour
 {
     static bool playStat = false;
+    static bool showState = true;
+
+    public static bool buttonClicked = false;
+
+    public GameObject toolPanel;
+    public GameObject Road;
+    public Text textref;
+
+    public List<Vector3> roadPosition;
+
     // Start is called before the first frame update
     void Awake()
     {
         Time.timeScale = 0;
         Debug.Log("Awake has called");
     }
-    void Start()
-    {
-        Debug.Log("start has called");
-    }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (buttonClicked){
+            placeObject();
+        }
     }
+
     public void onClickStartButton()
     {
         Debug.Log("start button is clicked");
@@ -30,24 +39,70 @@ public class UIManager : MonoBehaviour
         {
             Time.timeScale = 1;
             playStat = true;
+            textref.text = "Pause";
         }
         else
         {
             Time.timeScale = 0;
             playStat = false;
-        }
-    }
-    public void changeButtonText(Text textref)
-    {
-        if (playStat)
-            textref.text = "Pause";
-        else
             textref.text = "Start";
+        }
     }
     public void onClickResetButton()
     {
         Debug.Log("stop button is pushed");
         SceneManager.LoadScene( SceneManager.GetActiveScene().buildIndex );
         Time.timeScale = 0;
+    }//Just for testing. need to be rewritten
+
+    public void switchPanelShowState()
+    {
+        showState = !showState;
+        if (showState)
+        {
+            toolPanel.SetActive(true);
+        }
+        else
+        {
+            toolPanel.SetActive(false);
+        }
+    }
+    
+    public void placeObject()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Plane Plane = new Plane(Vector3.up, Vector3.zero);
+            Ray Ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            float entry;
+
+            if (Plane.Raycast(Ray, out entry))
+            {
+                roadPosition.Add(Ray.GetPoint(entry));
+            }
+        }
+        if (roadPosition.Count == 2)
+        {
+            Vector3 position = (roadPosition[1] + roadPosition[0]) / 2.0f;
+
+            Quaternion rotation = Quaternion.LookRotation(roadPosition[1] - roadPosition[0], Vector3.up);
+            rotation *= Quaternion.Euler(0, -90f, 0);
+
+            Instantiate(Road, position, rotation);
+
+            roadPosition.Clear();
+
+            buttonClicked = false;
+        }
+    }
+
+    public void onClickedButton1()
+    {
+        buttonClicked = true;
+    }
+
+    public enum ObjectType
+    {
+        Road, Source, Junction
     }
 }

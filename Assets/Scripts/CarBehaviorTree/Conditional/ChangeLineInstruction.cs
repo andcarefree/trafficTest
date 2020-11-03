3436d1d68b62a086de963a0d5a5be2abda1f490e
you-ran <youran@users.noreply.github.com>
2020-11-03 23:30:23 +0800

    修复一些错误

--- Assets/Scripts/CarBehaviorTree/Conditional/ChangeLineInstruction.cs
+++ Assets/Scripts/CarBehaviorTree/Conditional/ChangeLineInstruction.cs
@@ -1,8 +1,7 @@
 ﻿using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
-using BehaviorDesigner.Runtime;
 using BehaviorDesigner.Runtime.Tasks;
+
+//通过改变car.lineChange发出对车辆的换道指令
 public class ChangeLineInstruction : Conditional
 {
     Car car;
@@ -14,7 +13,20 @@ public class ChangeLineInstruction : Conditional
 
     public override TaskStatus OnUpdate()
     {
-        if (car.lineChange == true)
+        //即将驶入路口，不予换道
+        // TODO 考虑到曲线道路，判断车辆点与终点的直线长度可能会有问题
+        //可以改换到T参数判断？
+        if (car.lineChange == true && car.state == Car.State.inLine && Vector3.Distance(car.transform.position, car.line.points[car.line.points.Length - 1]) <= 14)
+        {
+            //重置换道指令
+            if (car.lineChange == true)
+            {
+                car.lineChange = false;
+            }
+            return TaskStatus.Failure;
+        }
+
+        if (car.state == Car.State.changing || (car.lineChange == true && car.state == Car.State.inLine))
         {
             return TaskStatus.Success;
         }
@@ -22,6 +34,6 @@ public class ChangeLineInstruction : Conditional
         {
             return TaskStatus.Failure;
         }
-        
+
     }
 }

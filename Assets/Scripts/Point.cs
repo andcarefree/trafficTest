79f4358692e17f7bac6c993e6d8e3bc8815c4c48
darkboy <zenglin12345@gmail.com>
2021-02-23 15:01:47 +0800

    bugfix

--- Assets/Scripts/Point.cs
+++ Assets/Scripts/Point.cs
@@ -5,17 +5,26 @@ using UnityEngine;
 public class Point : MonoBehaviour
 {
     private float scaleX, scaleY, scaleZ;
+    private Vector3 mouseOffset;
+    private Vector3 screenPoint;
 
     void Update()
     {
         // fix size 
         transform.localScale = new Vector3(0.5f / transform.parent.localScale.x, 0.5f / transform.parent.localScale.y, 0.5f / transform.parent.localScale.z);
     }
+
+    void OnMouseDown()
+    {
+        screenPoint = Camera.main.WorldToScreenPoint(transform.position);
+        mouseOffset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+    }
+
     void OnMouseDrag()
     {
         // Change Position
-        Vector3 selectPosition= Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        selectPosition.y = 0;
-        transform.position = selectPosition;
+        Vector3 currentScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+        Vector3 currentPosition = Camera.main.ScreenToWorldPoint(currentScreenPoint) + mouseOffset;
+        transform.position = currentPosition;
     }
 }

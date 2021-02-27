70cc8924942e0be1d12166c78267090b94ed2982
darkboy <zenglin12345@gmail.com>
2021-02-27 11:11:15 +0800

    add hints for buttons

--- Assets/Scripts/UI/Buttons/ButtonHandler.cs
+++ Assets/Scripts/UI/Buttons/ButtonHandler.cs
@@ -1,33 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ButtonHandler : MonoBehaviour
 {
-    public static List<Vector3> objectsPosition;
-
-    public GameObject roadPrefeb;
-    public GameObject carPrefeb;
-    public GameObject saveDialog;
-
-    public List<Vector3> roadPosition;
-    public List<GameObject> roadList;
+    [SerializeField] private TextMeshProUGUI statusBar;
+    [SerializeField] private GameObject roadPrefeb;
+    [SerializeField] private GameObject carPrefeb;
+    private GameObject[] roadList;
+    private Vector3[] roadPosition;
 
     void Awake()
     {
         Time.timeScale = 0;
-        
-        objectsPosition = new List<Vector3>();
+        roadList = new GameObject[2];
+        roadPosition = new Vector3[2];
     }
 
     private IEnumerator SetUpRoad()
     {
-        bool isDone = false;
-        while (!isDone)
+        int status = 1;
+
+        statusBar.SetText("请点击道路的第一个点（起点），按ESC键退出");
+        while (status == 1)
         {
             if (Input.GetKeyDown(KeyCode.Escape))
             {    
-                roadPosition.Clear();
+                statusBar.SetText("");
                 yield break;
             }
 
@@ -39,60 +39,101 @@ public class ButtonHandler : MonoBehaviour
 
                 if (Plane.Raycast(Ray, out entry))
                 {
-                    roadPosition.Add(Ray.GetPoint(entry));
+                    roadPosition[0] = Ray.GetPoint(entry);
                 }
-            }
-            if (roadPosition.Count == 2)
-            {
-                Vector3 position = (roadPosition[1] + roadPosition[0]) / 2.0f;
-                var scale = Vector3.Distance(roadPosition[1], roadPosition[0]) / 60.0f;
-
-                Quaternion rotation = Quaternion.LookRotation(roadPosition[1] - roadPosition[0], Vector3.up);
-                rotation *= Quaternion.Euler(0, -90f, 0);
-
-                GameObject newRoad = Instantiate(roadPrefeb, position, rotation);
-                newRoad.transform.localScale = new Vector3(scale, 1.0f, 1.0f);
-
-                roadPosition.Clear();
-                isDone = true;
+                status += 1;
             }
             yield return null;
         }
+
+        statusBar.SetText("请点击道路的第二个点（终点），按ESC键退出");
+        while (status == 2)
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {    
+                statusBar.SetText("");
+                yield break;
+            }
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                var Plane = new Plane(Vector3.up, Vector3.zero);
+                var Ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                float entry;
+
+                if (Plane.Raycast(Ray, out entry))
+                {
+                    roadPosition[1] = Ray.GetPoint(entry);
+                }
+                status += 1;
+            }
+            yield return null;
+        }
+
+        Vector3 position = (roadPosition[1] + roadPosition[0]) / 2.0f;
+        var scale = Vector3.Distance(roadPosition[1], roadPosition[0]) / 60.0f;
+
+        Quaternion rotation = Quaternion.LookRotation(roadPosition[1] - roadPosition[0], Vector3.up);
+        rotation *= Quaternion.Euler(0, -90f, 0);
+
+        GameObject newRoad = Instantiate(roadPrefeb, position, rotation);
+        newRoad.transform.localScale = new Vector3(scale, 1.0f, 1.0f);
+
+        statusBar.SetText("");
     }
 
     private IEnumerator ConnectLane()
     {
-        bool isDone = false;
-        while (!isDone)
+        int status = 1;
+
+        statusBar.SetText("请点击需要被连接的前一条车道， 按ESC退出");
+        while (status == 1)
         {
             if(Input.GetKey(KeyCode.Escape))
             {
-                roadList.Clear();
+                statusBar.SetText("");
                 yield break;
             }
             if(Input.GetMouseButtonDown(0))
             {
-                roadList.Add(this.SelectObjectOnClick());
-            }
-            if(roadList.Count == 2)
-            {
-                var nextRoad = roadList[1].GetComponentsInParent<Road>();
-                roadList[0].GetComponent<Line>().nextRoads = nextRoad;
-
-                roadList.Clear();
-                isDone = true;
+                roadList[1] = this.SelectObjectOnClick();
+                status += 1;
             }
             yield return null;
         }
+
+        statusBar.SetText("请点击需要被连接的后一条车道， 按ESC退出");
+        while (status == 2)
+        {
+            if(Input.GetKey(KeyCode.Escape))
+            {
+                statusBar.SetText("");
+                yield break;
+            }
+            if(Input.GetMouseButtonDown(0))
+            {
+                roadList[1] = this.SelectObjectOnClick();
+                status += 1;
+            }
+            yield return null;
+        }
+
+        var nextRoad = roadList[1].GetComponentsInParent<Road>();
+        roadList[0].GetComponent<Line>().nextRoads = nextRoad;
+
+        statusBar.SetText("");
     }
 
     private IEnumerator SetUpCarSource()
     {
         bool isDone = false;
+
+        statusBar.SetText("请点击需要设置为车辆源的道路");
         while (!isDone)
         {
             if (Input.GetKeyDown(KeyCode.Escape))
             {
+                statusBar.SetText("");
                 yield break;
             }
             if (Input.GetMouseButtonDown(0))
@@ -109,6 +150,8 @@ public class ButtonHandler : MonoBehaviour
             }       
             yield return null;
         }
+
+        statusBar.SetText("");
     }
 
     private GameObject SelectObjectOnClick()
@@ -132,17 +175,38 @@ public class ButtonHandler : MonoBehaviour
 
     public void OnGenerateRoadButtonClick()
     {
-        StartCoroutine(SetUpRoad());
+        if (Time.timeScale == 0)
+        {
+            StartCoroutine(SetUpRoad());
+        }
+        else
+        {
+            statusBar.SetText("正在播放中，请先停止再进行编辑");
+        }
     }
 
     public void OnLinkLaneButtonClick()
     {
-        StartCoroutine(ConnectLane());
+        if (Time.timeScale == 0)
+        {
+            StartCoroutine(ConnectLane());
+        }
+        else
+        {
+            statusBar.SetText("正在播放中，请先停止再进行编辑");
+        }
     }
 
     public void OnSetCarSourceButtonClick()
     {
-        StartCoroutine(SetUpCarSource());
+        if (Time.timeScale == 0)
+        {
+            StartCoroutine(SetUpCarSource());
+        }
+        else
+        {
+            statusBar.SetText("正在播放中，请先停止再进行编辑");
+        }
     }
 
     public void OnLoadModelButtonClick()

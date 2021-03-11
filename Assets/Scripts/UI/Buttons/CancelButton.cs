71178f5a623e0e845e13353a3140c7a0e9955467
darkboy <zenglin12345@gmail.com>
2021-03-11 20:23:46 +0800

    something modified

---
+++ Assets/Scripts/UI/Buttons/CancelButton.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CancelButton : MonoBehaviour
+{
+    public void OnCancelButtonClick()
+    {
+        transform.parent.parent.gameObject.SetActive(false);
+    }
+}

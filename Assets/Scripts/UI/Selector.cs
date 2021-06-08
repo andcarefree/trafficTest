1bfb9cfbef52d2433d0dc0111840b0df9d573b34
darrrrrrkboy <zenglin12345@gmail.com>
2021-06-08 23:49:54 +0800

    fix rect selection error at diffrent ratio

--- Assets/Scripts/UI/Selector.cs
+++ Assets/Scripts/UI/Selector.cs
@@ -8,7 +8,10 @@ public class Selector : MonoBehaviour
     public static Selector current;
     private Vector3 startPosition;
     private Vector3 endPosition;
-    
+
+    [SerializeField]
+    private RectTransform canvas;
+
     [SerializeField] 
     private RectTransform selectionBox;
 
@@ -25,68 +28,71 @@ public class Selector : MonoBehaviour
 
     void Update()
     {
-        RectangleSelection();
+        if (!EventSystem.current.IsPointerOverGameObject())
+        {
+            RectangleSelection();
+        }
     }
 
     // 框选游戏对象
     private void RectangleSelection()
     {
-        if(!EventSystem.current.IsPointerOverGameObject() & !ScrollBar.isGUIActive)
+        if(Input.GetMouseButtonDown(0))
         {
-            if(Input.GetMouseButtonDown(0))
+            startPosition = Input.mousePosition;
+        }
+        else if(Input.GetMouseButton(0))
+        {
+            endPosition = Input.mousePosition;
+
+            var xMax = Mathf.Max(endPosition.x, startPosition.x);
+            var yMax = Mathf.Max(endPosition.y, startPosition.y);
+            var xMin = Mathf.Min(endPosition.x, startPosition.x);
+            var yMin = Mathf.Min(endPosition.y, startPosition.y);
+            
+            var width = (endPosition.x - startPosition.x) *  canvas.rect.width / Screen.width;
+            var height = (endPosition.y - startPosition.y) * canvas.rect.height / Screen.height;
+
+            var positionX = startPosition.x * canvas.rect.width / Screen.width + width / 2;
+            var positionY = startPosition.y * canvas.rect.height / Screen.height + height / 2;
+
+            selectionBox.sizeDelta = new Vector2(Mathf.Abs(width), Mathf.Abs(height));
+            selectionBox.anchoredPosition = new Vector2(positionX, positionY);
+
+            if(!selectionBox.gameObject.activeInHierarchy)
+                selectionBox.gameObject.SetActive(true);
+            
+            foreach(var go in Selectable)
             {
-                startPosition = Input.mousePosition;
-            }
-            else if(Input.GetMouseButton(0))
-            {
-                endPosition = Input.mousePosition;
+                var position = Camera.main.WorldToScreenPoint(go.transform.position);
 
-                var xMax = Mathf.Max(endPosition.x, startPosition.x);
-                var yMax = Mathf.Max(endPosition.y, startPosition.y);
-                var xMin = Mathf.Min(endPosition.x, startPosition.x);
-                var yMin = Mathf.Min(endPosition.y, startPosition.y);
-                
-                var width = (endPosition.x - startPosition.x) * 1920f / Screen.width;
-                var height = (endPosition.y - startPosition.y) * 1080f / Screen.height;
-
-                selectionBox.sizeDelta = new Vector2(Mathf.Abs(width), Mathf.Abs(height));
-                selectionBox.anchoredPosition = new Vector2(startPosition.x * 1920f / Screen.width + width / 2, startPosition.y * 1080f / Screen.height + height / 2);
-
-                if(!selectionBox.gameObject.activeInHierarchy)
-                    selectionBox.gameObject.SetActive(true);
-                
-                foreach(var go in Selectable)
+                if(position.x > xMin && position.x < xMax && position.y > yMin && position.y < yMax)
                 {
-                    var position = Camera.main.WorldToScreenPoint(go.transform.position);
-
-                    if(position.x > xMin && position.x < xMax && position.y > yMin && position.y < yMax)
+                    if(!Selected.Contains(go))
                     {
-                        if(!Selected.Contains(go))
-                        {
-                            Selected.Add(go);
-                            go.GetComponent<Outline>().enabled = true;
-                        }              
-                    }
+                        Selected.Add(go);
+                        go.GetComponent<Outline>().enabled = true;
+                    }              
                 }
             }
-            else if(Input.GetKey(KeyCode.Escape))
+        }
+        else if(Input.GetKey(KeyCode.Escape))
+        {
+            foreach(GameObject gameObject in Selectable)
             {
-                foreach(GameObject gameObject in Selectable)
-                {
-                    if(Selected.Contains(gameObject))
-                    {    
-                        gameObject.GetComponent<Outline>().enabled = false;
-                        Selected.Remove(gameObject);
-                    }
+                if(Selected.Contains(gameObject))
+                {    
+                    gameObject.GetComponent<Outline>().enabled = false;
+                    Selected.Remove(gameObject);
                 }
+            }
 
-            }
-            else
+        }
+        else
+        {
+            if(selectionBox.gameObject.activeInHierarchy)
             {
-                if(selectionBox.gameObject.activeInHierarchy)
-                {
-                    selectionBox.gameObject.SetActive(false);
-                }
+                selectionBox.gameObject.SetActive(false);
             }
         }
     }
